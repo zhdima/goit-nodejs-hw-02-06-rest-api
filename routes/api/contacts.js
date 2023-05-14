7c43b5d1102cd3b34b13c62e2f09825340462cb1
zhdima <zhdima@gmail.com>
2023-05-14 13:02:42 +0300

    done

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,25 +1,21 @@
-const express = require('express')
+const express = require("express");
 
-const router = express.Router()
+const contactsController = require("../../controllers/contacts-controller");
 
-router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+const schemas = require("../../schemas/contacts-schemas");
 
-router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+const { validateBody } = require("../../decorators");
 
-router.post('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+const router = express.Router();
 
-router.delete('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.get("/", contactsController.listContacts);
 
-router.put('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.get("/:id", contactsController.getContactById);
 
-module.exports = router
+router.post("/", validateBody(schemas.contactAddSchema), contactsController.addContact);
+
+router.put("/:id", validateBody(schemas.contactUpdateSchema), contactsController.updateContact)
+
+router.delete("/:id", contactsController.removeContact)
+
+module.exports = router;
