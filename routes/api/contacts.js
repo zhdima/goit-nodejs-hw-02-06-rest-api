756debf61a00dd50792d0855ebd2e263cb0a800a
zhdima <zhdima@gmail.com>
2023-05-20 12:15:40 +0300

    add isValidId

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -5,17 +5,18 @@ const contactsController = require("../../controllers/contacts-controller");
 const schemas = require("../../schemas/contacts-schemas");
 
 const { validateBody } = require("../../decorators");
+const { isValidId } = require("../../helpers");
 
 const router = express.Router();
 
 router.get("/", contactsController.listContacts);
 
-router.get("/:id", contactsController.getContactById);
+router.get("/:id", isValidId, contactsController.getContactById);
 
 router.post("/", validateBody(schemas.contactAddSchema), contactsController.addContact);
 
-router.put("/:id", validateBody(schemas.contactUpdateSchema), contactsController.updateContact)
+router.put("/:id", isValidId, validateBody(schemas.contactUpdateSchema), contactsController.updateContact)
 
-router.delete("/:id", contactsController.removeContact)
+router.delete("/:id", isValidId, contactsController.removeContact)
 
 module.exports = router;
