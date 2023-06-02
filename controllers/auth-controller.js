b96194c91ed429e58234f037a8b7c7f0c8b21fa4
zhdima <zhdima@gmail.com>
2023-06-02 21:50:26 +0300

    add avatarURL, gravatar

--- controllers/auth-controller.js
+++ controllers/auth-controller.js
@@ -1,5 +1,6 @@
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
+const gravatar = require("gravatar");
 
 const { User } = require("../models/users");
 
@@ -17,8 +18,9 @@ const register = async (req, res) => {
   }
 
   const hashPassword = await bcrypt.hash(password, 10);
+  const avatarURL = gravatar.url(email);
 
-  const result = await User.create({ ...req.body, password: hashPassword });
+  const result = await User.create({ ...req.body, password: hashPassword, avatarURL });
 
   res.status(201).json({
     user: {
