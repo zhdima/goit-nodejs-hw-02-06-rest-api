7834d6f42ea2e71c7cfb52d6ad6a08f8666d0de1
zhdima <zhdima@gmail.com>
2023-06-10 15:21:04 +0300

    check verified login

--- controllers/auth-controller.js
+++ controllers/auth-controller.js
@@ -48,6 +48,10 @@ const login = async (req, res) => {
     throw HttpError(401, "Email or password is wrong");
   }
 
+  if (!user.verify) {
+    throw HttpError(401, "Email is not verified");
+  }
+
   const passwordCompare = await bcrypt.compare(password, user.password);
   if (!passwordCompare) {
     throw HttpError(401, "Email or password is wrong");
